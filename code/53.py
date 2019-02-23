0e073e8bf5b7520797ee9ca845c36b6225d84061
rahulcs754 <rahul.shukla754@gmail.com>
2019-02-23 22:46:08 +0530

    add code again

---
+++ code/53.py
@@ -0,0 +1,16 @@
+# A Python 2.7 program to demonstrate use of 
+# "/" for integers 
+print 5/2
+print -5/2
+
+# A Python 2.7 program to demonstrate use of 
+# "/" for floating point numbers 
+print 5.0/2
+print -5.0/2
+
+# A Python 2.7 program to demonstrate use of 
+# "//" for both integers and floating points 
+print 5//2
+print -5//2
+print 5.0//2
+print -5.0//2
